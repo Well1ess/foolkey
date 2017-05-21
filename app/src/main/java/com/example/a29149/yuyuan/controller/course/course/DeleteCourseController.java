2780195c5f436b17f4d61b3c4d20b22154ed6bb9
葛尧 <geyao2012@gmail.com>
2017-05-21 13:57:26 +0800

    新建了一些可能马上要用的接口

---
+++ app/src/main/java/com/example/a29149/yuyuan/controller/course/course/DeleteCourseController.java
@@ -0,0 +1,29 @@
+package com.example.a29149.yuyuan.controller.course.course;
+
+import com.example.a29149.yuyuan.Util.HttpSender;
+import com.example.a29149.yuyuan.controller.AbstractController;
+
+import org.json.JSONObject;
+
+/**
+ *
+ * 删除某个课程
+ * 需要courseId
+ * Created by geyao on 2017/5/21.
+ */
+
+public class DeleteCourseController extends AbstractController{
+
+    private static String url = address + "/aes/course/delete";
+
+    public static String execute(String courseId){
+        try {
+            JSONObject jsonObject = getJSON();
+            jsonObject.put("courseId", courseId);
+            return HttpSender.send(url, jsonObject);
+        }catch (Exception e){
+            e.printStackTrace();
+            return failJSON();
+        }
+    }
+}

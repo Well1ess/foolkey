dcb4286a822de19f3d1a93cd26ac43fa2aef2fe4
葛尧 <geyao2012@gmail.com>
2017-05-22 13:33:57 +0800

    完成了splash activity的显示效果

---
+++ app/src/main/java/com/example/a29149/yuyuan/Main/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.example.a29149.yuyuan.Main;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.a29149.yuyuan.R;
+
+/**
+ * Created by geyao on 2017/5/22.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_splash);
+    }
+}

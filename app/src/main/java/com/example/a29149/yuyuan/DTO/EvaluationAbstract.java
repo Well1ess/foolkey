3ca4f6055ae14e4b4cdc3eee99e3e106d03d81ed
耿瑞 <gr2016@mail.ustc.edu.cn>
2017-05-24 20:27:04 +0800

    评价DTO

--- app/src/main/java/com/example/a29149/yuyuan/DTO/EvaluationAbstract.java
+++ app/src/main/java/com/example/a29149/yuyuan/DTO/EvaluationAbstract.java
@@ -24,4 +24,64 @@ public class EvaluationAbstract {
 
     //分数
     private Double score;
+
+    @Override
+    public String toString() {
+        return "EvaluationAbstract{" +
+                "id=" + id +
+                ", creatorId=" + creatorId +
+                ", acceptor_id=" + acceptor_id +
+                ", orderId=" + orderId +
+                ", evaluationStateEnum=" + evaluationStateEnum +
+                ", score=" + score +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCreatorId() {
+        return creatorId;
+    }
+
+    public void setCreatorId(Long creatorId) {
+        this.creatorId = creatorId;
+    }
+
+    public Long getAcceptor_id() {
+        return acceptor_id;
+    }
+
+    public void setAcceptor_id(Long acceptor_id) {
+        this.acceptor_id = acceptor_id;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public EvaluationStateEnum getEvaluationStateEnum() {
+        return evaluationStateEnum;
+    }
+
+    public void setEvaluationStateEnum(EvaluationStateEnum evaluationStateEnum) {
+        this.evaluationStateEnum = evaluationStateEnum;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
 }
--- app/src/main/java/com/example/a29149/yuyuan/DTO/EvaluationCourseDTO.java
+++ app/src/main/java/com/example/a29149/yuyuan/DTO/EvaluationCourseDTO.java
@@ -4,5 +4,81 @@ package com.example.a29149.yuyuan.DTO;
  * Created by GR on 2017/5/24.
  */
 
-public class EvaluationCourseDTO {
+public class EvaluationCourseDTO  extends EvaluationAbstract {
+
+    //评价的文字内容
+    private String content;
+
+    //课程id
+    private Long courseId;
+
+    //图片1地址
+    private String pic1Path;
+
+    private String pic2Path;
+
+    private String pic3Path;
+
+    private String pic4Path;
+
+
+    @Override
+    public String toString() {
+        return "EvaluationCourseDTO{" +
+                "content='" + content + '\'' +
+                ", courseId=" + courseId +
+                ", pic1Path='" + pic1Path + '\'' +
+                ", pic2Path='" + pic2Path + '\'' +
+                ", pic3Path='" + pic3Path + '\'' +
+                ", pic4Path='" + pic4Path + '\'' +
+                '}';
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Long getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(Long courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getPic1Path() {
+        return pic1Path;
+    }
+
+    public void setPic1Path(String pic1Path) {
+        this.pic1Path = pic1Path;
+    }
+
+    public String getPic2Path() {
+        return pic2Path;
+    }
+
+    public void setPic2Path(String pic2Path) {
+        this.pic2Path = pic2Path;
+    }
+
+    public String getPic3Path() {
+        return pic3Path;
+    }
+
+    public void setPic3Path(String pic3Path) {
+        this.pic3Path = pic3Path;
+    }
+
+    public String getPic4Path() {
+        return pic4Path;
+    }
+
+    public void setPic4Path(String pic4Path) {
+        this.pic4Path = pic4Path;
+    }
 }

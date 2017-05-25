f0610c9642480aeb5495558ffc0dd4498a312310
葛尧 <geyao2012@gmail.com>
2017-05-25 20:16:17 +0800

    更新了一个dto

---
+++ app/src/main/java/com/example/a29149/yuyuan/DTO/CourseWithTeacherSTCDTO.java
@@ -0,0 +1,40 @@
+package com.example.a29149.yuyuan.DTO;
+
+/**
+ * Created by geyao on 2017/5/25.
+ */
+
+public class CourseWithTeacherSTCDTO {
+
+
+        //课程信息
+        private CourseDTO courseDTO;
+        //老师信息
+        private TeacherAllInfoDTO teacherAllInfoDTO;
+
+        @Override
+        public String toString() {
+            return "CourseWithTeacherSTCDTO{" +
+                    "courseDTO=" + courseDTO +
+                    ", teacherAllInfoDTO=" + teacherAllInfoDTO +
+                    '}';
+        }
+
+        public CourseDTO getCourseDTO() {
+            return courseDTO;
+        }
+
+        public void setCourseDTO(CourseDTO courseDTO) {
+            this.courseDTO = courseDTO;
+        }
+
+        public TeacherAllInfoDTO getTeacherAllInfoDTO() {
+            return teacherAllInfoDTO;
+        }
+
+        public void setTeacherAllInfoDTO(TeacherAllInfoDTO teacherAllInfoDTO) {
+            this.teacherAllInfoDTO = teacherAllInfoDTO;
+        }
+
+
+}

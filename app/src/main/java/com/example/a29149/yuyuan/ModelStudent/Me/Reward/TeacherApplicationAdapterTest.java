0ab9b39980c2cd188bf8b14e251e4d1b6cd85e64
葛尧 <geyao2012@gmail.com>
2017-06-23 16:38:12 +0800

    写了RecyclerView的adapter，展示申请的老师头像

---
+++ app/src/main/java/com/example/a29149/yuyuan/ModelStudent/Me/Reward/TeacherApplicationAdapterTest.java
@@ -0,0 +1,147 @@
+package com.example.a29149.yuyuan.ModelStudent.Me.Reward;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
+import com.example.a29149.yuyuan.AbstractObject.UserPhotoAdapter;
+import com.example.a29149.yuyuan.AbstractObject.YYRecycleViewBaseAdapter;
+import com.example.a29149.yuyuan.DTO.ApplicationRewardWithTeacherSTCDTO;
+import com.example.a29149.yuyuan.DTO.TeacherAllInfoDTO;
+import com.example.a29149.yuyuan.R;
+import com.example.a29149.yuyuan.Util.StringUtil;
+import com.example.a29149.yuyuan.business_object.com.PictureInfoBO;
+import com.example.resource.util.image.GlideCircleTransform;
+
+import java.util.List;
+
+/**
+ * Author:       geyao
+ * Date:         2017/6/15
+ * Email:        gy2016@mail.ustc.edu.cn
+ * Description:  展示用户头像的子类
+ * 此处用来，学生查看老师对其悬赏的申请，一个老师就是一个头像
+ * Created by geyao on 2017/6/15.
+ */
+
+public class TeacherApplicationAdapterTest extends YYRecycleViewBaseAdapter<ApplicationRewardWithTeacherSTCDTO> {
+
+    private static final String TAG = "TeacherApplicationAdapt";
+
+    //图片加载器
+    private RequestManager glide;
+
+
+    /**
+     * @param dataList 数据
+     * @param context  上下文
+     *
+     * @discription 构造函数
+     * @author 29149
+     * @time 2017/6/10 15:12
+     */
+    public TeacherApplicationAdapterTest(List<ApplicationRewardWithTeacherSTCDTO> dataList, Context context) {
+        super(dataList, context);
+    }
+
+    /**
+     * 构造函数
+     *
+     * @param mContext 上下文
+     */
+    public TeacherApplicationAdapterTest(Context mContext) {
+        super(mContext);
+    }
+
+
+    /**
+     *
+     * Author:       geyao
+     * Date:         2017/6/23 下午4:22
+     * Email:        gy2016@mail.ustc.edu.cn
+     * Description:  获取ViewHolder，感觉返回值是下个函数的参数
+     * @param parent
+     * @param viewType
+     * @return
+     */
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.gridview_reply_item_layout, null);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                view.setLayoutParams(lp);
+        return new PersonViewHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        //获取老师信息
+        TeacherAllInfoDTO teacherAllInfoDTO = getDataList().get(position).getTeacherAllInfoDTO();
+        //转换为本类的viewHolder
+        PersonViewHolder viewHolder = (PersonViewHolder) holder;
+        //绑定数据
+        viewHolder.mNickedName.setText(StringUtil.subString(teacherAllInfoDTO.getNickedName(), 8));
+        viewHolder.mPrestige.setText( teacherAllInfoDTO.getPrestige() );
+
+        glide = Glide.with(mContext);
+        glide.load( PictureInfoBO.getOnlinePhoto(teacherAllInfoDTO.getUserName() ) )
+                .placeholder(R.drawable.place_holder_grey)
+                .transform(new GlideCircleTransform(mContext))
+                .into(viewHolder.mPhoto);
+    }
+
+    /**
+     *
+     * Author:       geyao
+     * Date:         2017/6/23 下午4:19
+     * Email:        gy2016@mail.ustc.edu.cn
+     * Description:  RecyclerView的专属ViewHolder
+     */
+    class PersonViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
+
+        //头像
+        public ImageView mPhoto;
+        //昵称
+        public TextView mNickedName;
+        //声望
+        public TextView mPrestige;
+
+        public PersonViewHolder(View itemView) {
+            super(itemView);
+            mPhoto = (ImageView) itemView.findViewById(R.id.iv_photo);
+            mNickedName = (TextView) itemView.findViewById(R.id.tv_teacherName);
+            mPrestige = (TextView) itemView.findViewById(R.id.tv_prestige);
+        }
+
+        /**
+         * Called when a view has been clicked.
+         *
+         * @param v The view that was clicked.
+         */
+        @Override
+        public void onClick(View v) {
+            //这里可以设置点击事件
+        }
+
+        /**
+         * Called when a view has been clicked and held.
+         *
+         * @param v The view that was clicked and held.
+         *
+         * @return true if the callback consumed the long click, false otherwise.
+         */
+        @Override
+        public boolean onLongClick(View v) {
+            //长按点击事件
+            return false;
+        }
+    }
+}
